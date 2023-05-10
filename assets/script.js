196d2ae2836d4e9405ba4226990e06eff49cee29
newprice247 <newprice247@gmail.com>
2023-05-10 15:13:04 -0400

    completing the git flow as per lesson 3.4.5

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,37 @@
+var title = 'Here are the topics we learned through Prework:';
+
+console.log(title);
+
 var topics = ['HTML' , 'CSS', 'Git', 'JavaScript']
 
-for (i = 0; i < topics.length; i++) {
+function listTopics() {
+    for (i = 0; i < topics.length; i++) {
     console.log(topics[i]);
-
+  };
 };
+
+listTopics();
+
+function selectTopic() {
+    let topic = topics[Math.floor(Math.random() * topics.length)]
+    if (topic === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (topic === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (topic === 'Git') {
+      console.log("Let's study Git!");
+    } else if (topic === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+  }
+
+
+function topicChooser() {
+    console.log('Which topic should we study first?');
+    selectTopic();
+
+}
+
+topicChooser();
